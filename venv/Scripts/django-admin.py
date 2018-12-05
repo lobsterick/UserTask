3ceb1c0bd7238284b0fc2ede3d5610de2769ca43
lobsterick <lobsterick@gmail.com>
2018-12-05 12:03:38 +0100

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\raq195\Desktop\CS50W_Projects\UserTask\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
