dba6a4838338d14a3257741880dd161fc01730fc
lobsterick <lobsterick@gmail.com>
2018-12-05 12:13:15 +0100

    Add models for Website, WebsiteCategory and WebPage

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class WebsiteCategory(models.Model):
+    name = models.CharField(max_length=1000)
+    description = models.CharField(max_length=10000)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    count = models.IntegerField()
+
+class Website(models.Model):
+    url = models.CharField(max_length=1000)
+    title = models.CharField(max_length=1000)
+    meta_description = models.CharField(max_length=1000)
+    alexa_rank = models.IntegerField()
+    category = models.ForeignKey(WebsiteCategory, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+class WebPage(models.Model):
+    website = models.ForeignKey(Website, on_delete=models.CASCADE)
+    url = models.CharField(max_length=1000, unique=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=1000)
+    meta_description = models.CharField(max_length=10000)
