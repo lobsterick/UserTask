163b580976bbf4b5bf131379e7b368b165777eee
lobsterick <lobsterick@gmail.com>
2018-12-05 12:35:54 +0100

    First working prototype of site

--- scraper/views.py
+++ scraper/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import *
 
-# Create your views here.
+
+class WebsiteListView(ListView):
+    queryset = Website.objects.all()
+    template_name = 'scraper/website_list.html'
+    context_object_name = "website_list"
+    paginate_by = 8
+
+
+class WebsiteDetailView(DetailView):
+    model = Website
